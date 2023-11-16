8de16e3997e6687bcb9524daf7d0ea7aff681234
Ana -rr <102273751+anatoly-rap@users.noreply.github.com>
2023-11-16 16:27:53 -0700

    Update trip.py
    refactor

--- trip.py
+++ trip.py
@@ -1,6 +1,9 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, QLabel, QSpacerItem, QSizePolicy
 from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, QLabel
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtCore import Qt, QSize
 import plotly.express as px
 import pandas as pd
 
@@ -30,15 +33,65 @@ def fig_to_HTML(fig):
     plot_html = fig.to_html(include_plotlyjs='cdn')
     return plot_html
 
+def calculate_metrics(data):
+    min_speed = data['SpeedMPH'].min()
+    max_speed = data['SpeedMPH'].max()
+    avg_speed = data['SpeedMPH'].mean()
+    return min_speed, max_speed, avg_speed
+
+def gen_speed_time_graph(data, min_speed, max_speed, avg_speed):
+    fig = px.line(data, x='Time', y='SpeedMPH', title=f'Speed Over Time (Min: {min_speed} MPH, Max: {max_speed} MPH, Avg: {avg_speed} MPH)')
+    fig.update_layout(xaxis_title='Time', yaxis_title='Speed (MPH)')
+    return fig
+
+def combine_plots_to_html(map_fig, speed_fig):
+    map_html = map_fig.to_html(full_html=False, include_plotlyjs='cdn')
+    speed_html = speed_fig.to_html(full_html=False, include_plotlyjs=False)
+    combined_html = f"<html><head></head><body><div style='display:flex;justify-content:space-between;'>{map_html}{speed_html}</div></body></html>"
+    return combined_html
+
+def setup_header_label():
+    header_label = QLabel("Commute Optimizer")
+    header_label.setAlignment(Qt.AlignCenter)
+    size_policy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
+    header_label.setSizePolicy(size_policy)
+    return header_label
+
+def setup_map_view(data):
+    map_fig = gen_plot(data)
+    map_view = QWebEngineView()
+    map_view.setFixedSize(QSize(600, 650))
+    map_view.setHtml(map_fig.to_html(include_plotlyjs='cdn'))
+    return map_view
+
+def setup_speed_view(data):
+    min_speed, max_speed, avg_speed = calculate_metrics(data)
+    speed_fig = gen_speed_time_graph(data, min_speed, max_speed, avg_speed)
+    speed_view = QWebEngineView()
+    speed_view.setFixedSize(QSize(600, 650))
+    speed_view.setHtml(speed_fig.to_html(include_plotlyjs='cdn'))
+    return speed_view
+
 def main():
     app = QApplication(sys.argv)
     main_window = QMainWindow()
-    main_window.resize(800, 600)
-    web_engine_view = QWebEngineView(main_window)
+    main_widget = QWidget()
+    grid_layout = QGridLayout(main_widget)
+
+    header_label = setup_header_label()
     data = load_trip()
-    fig = gen_plot(data)
-    web_engine_view.setHtml(fig_to_HTML(fig))
-    main_window.setCentralWidget(web_engine_view)
+    map_view = setup_map_view(data)
+    speed_view = setup_speed_view(data)
+
+    grid_layout.addWidget(header_label, 0, 0, 1, 2)  # Span across the first row
+    grid_layout.addWidget(map_view, 1, 0)  # Position (row 1, column 0)
+    grid_layout.addWidget(speed_view, 1, 1)  # Position (row 1, column 1)
+    spacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+    grid_layout.addItem(spacer, 2, 0, 1, 2)
+
+    main_widget.setLayout(grid_layout)
+    main_window.setCentralWidget(main_widget)
+    main_window.setFixedSize(QSize(1200, 650))
     main_window.show()
     sys.exit(app.exec_())
 
