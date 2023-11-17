98d54277ae8a833ca10fcaa73b7baadd87012f08
Ana -rr <102273751+anatoly-rap@users.noreply.github.com>
2023-11-17 13:39:24 -0700

    Update trip.py
    refac

--- trip.py
+++ trip.py
@@ -2,14 +2,13 @@ import sys
 from PyQt5.QtWidgets import  QSpacerItem, QSizePolicy
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, QLabel
-from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtCore import Qt, QSize
 import plotly.express as px
 import pandas as pd
 
 def load_trip():
     #any dataset you gathered, which must include lat,long, and speed. 
-    file_path = './trip1.xlsx'
+    file_path = './com_1.xlsx'
     data = pd.read_excel(file_path)
     return data
 
@@ -29,18 +28,12 @@ def gen_plot(data):
     )
     return fig
 
-def fig_to_HTML(fig):
-    plot_html = fig.to_html(include_plotlyjs='cdn')
-    return plot_html
+def fig_to_HTML(fig): return fig.to_html(include_plotlyjs='cdn')
 
-def calculate_metrics(data):
-    min_speed = data['SpeedMPH'].min()
-    max_speed = data['SpeedMPH'].max()
-    avg_speed = data['SpeedMPH'].mean()
-    return min_speed, max_speed, avg_speed
+def calculate_metrics(data): return data['SpeedMPH'].min(), data['SpeedMPH'].max(), data['SpeedMPH'].mean()
 
 def gen_speed_time_graph(data, min_speed, max_speed, avg_speed):
-    fig = px.line(data, x='Time', y='MPH', title=f'Speed Over Time (Min: {min_speed} MPH, Max: {max_speed} MPH, Avg: {avg_speed} MPH)')
+    fig = px.line(data, x='Time', y='SpeedMPH', title=f'Speed Over Time (Min: {min_speed} MPH, Max: {max_speed} MPH, Avg: {avg_speed} MPH)')
     fig.update_layout(xaxis_title='Time', yaxis_title='Speed (MPH)')
     return fig
 
@@ -53,8 +46,7 @@ def combine_plots_to_html(map_fig, speed_fig):
 def setup_header_label():
     header_label = QLabel("Commute Optimizer")
     header_label.setAlignment(Qt.AlignCenter)
-    size_policy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
-    header_label.setSizePolicy(size_policy)
+    header_label.setSizePolicy(QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed))
     return header_label
 
 def setup_map_view(data):
@@ -65,10 +57,9 @@ def setup_map_view(data):
 
 def setup_speed_view(data):
     min_speed, max_speed, avg_speed = calculate_metrics(data)
-    speed_fig = gen_speed_time_graph(data, min_speed, max_speed, avg_speed)
     speed_view = QWebEngineView()
     speed_view.setFixedSize(QSize(600, 650))
-    speed_view.setHtml(speed_fig.to_html(include_plotlyjs='cdn'))
+    speed_view.setHtml(gen_speed_time_graph(data, min_speed, max_speed, avg_speed).to_html(include_plotlyjs='cdn'))
     return speed_view
 
 def main():
@@ -76,13 +67,9 @@ def main():
     main_window = QMainWindow()
     main_widget = QWidget()
     grid_layout = QGridLayout(main_widget)
-    grid_layout.addWidget(
-        setup_header_label(), 0, 0, 1, 2)
-    grid_layout.addWidget(
-        setup_map_view(
-            load_trip()), 1, 0)
-    grid_layout.addWidget(
-        setup_speed_view(load_trip()), 1, 1) 
+    grid_layout.addWidget(setup_header_label(), 0, 0, 1, 2)
+    grid_layout.addWidget(setup_map_view(load_trip()), 1, 0)
+    grid_layout.addWidget(setup_speed_view(load_trip()), 1, 1) 
     grid_layout.addItem(QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding), 2, 0, 1, 2)
     main_widget.setLayout(grid_layout)
     main_window.setCentralWidget(main_widget)
